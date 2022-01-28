7766d5387e7098f191174ab2075a07449da43892
Thom Hammond <thomhammond@Thoms-MacBook-Air.local>
2022-01-28 15:30:57 -0800

    Completed basic implementation of Percolation. Need to add documentation, testing, and address 'backwash' bug. Added Visualizer and resources from Princeton Algorithms course

--- src/main/java/Percolation.java
+++ src/main/java/Percolation.java
@@ -3,41 +3,54 @@ import edu.princeton.cs.algorithms.WeightedQuickUnionUF;
 public class Percolation {
     private int gridScale;
     private int gridSize;
+
     private WeightedQuickUnionUF weightedQuickUnionUF;
+
     private boolean[] openSites;
     private int numOpenSites;
 
+    private int virtualTopSite;
+    private int virtualBottomSite;
 
     public Percolation(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("N must be greater than 0");
+        }
+
         this.gridScale = n;
         this.gridSize = n * n;
+
+        // Initialize with size = gridSize + 2 to account for virtual sites
         this.weightedQuickUnionUF = new WeightedQuickUnionUF(gridSize + 2);
         this.openSites = new boolean[gridSize + 2];
+
         this.numOpenSites = 0;
 
-        // Open and connect virtual sites
-        this.openSites[0] = true;
+        this.virtualTopSite = 0;
+        this.virtualBottomSite = gridSize + 1;
+
+        // Open and connect virtual top site
+        this.openSites[virtualTopSite] = true;
         for (int i = 1; i <= gridScale; i++) {
-            weightedQuickUnionUF.union(0, i);
+            weightedQuickUnionUF.union(virtualTopSite, i);
         }
-        this.openSites[gridSize + 1] = true;
+
+        // Open and connect virtual bottom site
+        this.openSites[virtualBottomSite] = true;
         for (int j = gridSize - gridScale + 1; j <= gridSize; j++) {
-            weightedQuickUnionUF.union(gridSize + 1, j);
+            weightedQuickUnionUF.union(virtualBottomSite, j);
         }
 
     }
 
-    public void open(int row, int col) throws  IndexOutOfBoundsException {
+    public void open(int row, int col) {
+        // Will throw exception if row and col do not convert to a valid index
         int index = xyTo1D(row, col);
-
-        // Should this be thrown in xyTo1D method call?
-        if (index < 1 || index > gridSize) {
-            throw new IndexOutOfBoundsException("Invalid index");
-        }
 
         openSites[index] = true;
         numOpenSites++;
 
+        // If site is on the border of the grid, ignore neighbor indices outside of grid bounds
         int above = index <= gridScale ? -1 : xyTo1D(row - 1, col);
         int below = index > gridSize - gridScale ? -1 : xyTo1D(row + 1, col);
         int left = index % gridScale == 1 ? -1 : index - 1;
@@ -57,16 +70,91 @@ public class Percolation {
         }
     }
 
-    private int xyTo1D(int row, int col) {
-        return (row - 1) * gridScale + col;
+    public boolean isOpen(int row, int col) {
+        // Will throw exception if row and col do not convert to a valid index
+        int index = xyTo1D(row, col);
+
+        return openSites[index];
+    }
+
+    public boolean isFull(int row, int col) {
+        // Will throw exception if row and col do not convert to a valid index
+        int index = xyTo1D(row, col);
+
+        boolean isOpen = isOpen(row, col);
+        boolean isConnected = weightedQuickUnionUF.find(index) == weightedQuickUnionUF.find(virtualTopSite);
+
+        return isOpen && isConnected;
+    }
+
+    public int numberOfOpenSites() {
+        return numOpenSites;
+    }
+
+    public boolean percolates() {
+        return weightedQuickUnionUF.find(virtualTopSite) == weightedQuickUnionUF.find(virtualBottomSite);
+    }
+
+    private int xyTo1D(int row, int col) throws IndexOutOfBoundsException {
+        int index = (row - 1) * gridScale + col;
+
+        if (index < 1 || index > gridSize) {
+            throw new IndexOutOfBoundsException("Invalid index");
+        }
+
+        return index;
     }
 
     public static void main(String[] args) {
         Percolation percolation = new Percolation(3);
+
+        System.out.println();
+        System.out.println("System should NOT percolate");
+        System.out.println("System percolates: " + percolation.percolates());
+
+        System.out.println();
+        System.out.println("Row 1, Col 1 should be closed");
+        System.out.println("Row 1, Col 1 is open: " + percolation.isOpen(1, 1));
+
         percolation.open(1, 1);
-        percolation.open(1, 2);
+        System.out.println();
+        System.out.println("Row 1, Col 1 should be open");
+        System.out.println("Row 1, Col 1 is open: " + percolation.isOpen(1, 1));
+
+        System.out.println();
+        System.out.println("Row 2, Col 2 should be closed");
+        System.out.println("Row 2, Col 2 is open: " + percolation.isOpen(2, 2));
+
+        percolation.open(2, 2);
+        System.out.println();
+        System.out.println("Row 2, Col 2 should be open");
+        System.out.println("Row 2, Col 2 is open: " + percolation.isOpen(2, 2));
+
+        System.out.println();
+        System.out.println("System should NOT percolate");
+        System.out.println("System percolates: " + percolation.percolates());
+
+        System.out.println();
+        System.out.println("Row 3, Col 2 should be closed");
+        System.out.println("Row 3, Col 2 is open: " + percolation.isOpen(3, 2));
+
+        percolation.open(3, 2);
+        System.out.println();
+        System.out.println("Row 3, Col 2 should be open");
+        System.out.println("Row 3, Col 2 is open: " + percolation.isOpen(3, 2));
+
+        System.out.println();
+        System.out.println("Row 2, Col 1 should be closed");
+        System.out.println("Row 2, Col 1 is open: " + percolation.isOpen(2, 1));
+
         percolation.open(2, 1);
-        percolation.open(3, 3);
+        System.out.println();
+        System.out.println("Row 2, Col 1 should be open");
+        System.out.println("Row 2, Col 1 is open: " + percolation.isOpen(2, 1));
+
+        System.out.println();
+        System.out.println("System should percolate");
+        System.out.println("System percolates: " + percolation.percolates());
     }
 
 }
